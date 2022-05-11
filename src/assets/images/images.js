75f33366c6f4178307740afa8c2c999c92cf0638
phanison898 <phanison898@gmail.com>
2022-05-11 14:29:26 +0530

    updated linked-in see jobs image url

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -5,4 +5,4 @@ export const LinkedInLove = "https://static-exp1.licdn.com/sc/h/7fx9nkd7mx8avdpq
 export const LinkedInApplaud = "https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f";
 
 export const LinkedInJobAdd =
-  "https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg";
+  "https://firebasestorage.googleapis.com/v0/b/phanison-linkedin-clone.appspot.com/o/posts%2F6e2b9539-a772-4515-8557-0b0622b7a94f?alt=media&token=e98ffdd3-be70-4c91-82b7-82a750b297bc";
